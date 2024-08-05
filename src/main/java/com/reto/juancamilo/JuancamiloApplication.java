1703e661598577a07b98a7724b0b8ebeb569155a
Juan Camilo Noriega Hoyos <jcnorieg@bancolombia.com.co>
2024-08-04 21:20:04 -0500

    carga proyecto

---
+++ src/main/java/com/reto/juancamilo/JuancamiloApplication.java
@@ -0,0 +1,13 @@
+package com.reto.juancamilo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuancamiloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JuancamiloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reto/juancamilo/JuancamiloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reto.juancamilo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuancamiloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
